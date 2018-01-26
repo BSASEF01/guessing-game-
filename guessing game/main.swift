f7799c69053b6940240dfe49ca6de21c83a48e35
Bersabeh Asefa <bersabehasefa@Interapts-MacBook-Pro-8.local>
2018-01-26 16:45:52 -0500

    finished guessing game!

--- guessing game/main.swift
+++ guessing game/main.swift
@@ -8,24 +8,54 @@
 
 import Foundation
 
+
 //Generate a random number
 
-var randemnumber = arc4random_uniform(100)
+var randemnumber = Int(arc4random_uniform(100))
+var guess: Int?
+var tries = 1
 
-//Ask user input
-
-//Take user input
-
-//Determine wrong or ( Loop until correct )
-
-//If correct ask if they want to play again
-
-// If incorrect tell them the correct answer and ask them to play agin
-
-
-
+//print(randemnumber)
+var playAgain = "yes or okay"
+while playAgain == "yes or okay" {
+print("pleas guess a number.")
+guess = Int(readLine()!)
+while  guess != randemnumber && tries < 5{
+    if randemnumber > guess! {
+        print("The number was higher!")
+    } else if randemnumber < guess! {
+        print("The number was lower")
+    }
+    print("guess again")
+    guess = Int(readLine()!)!
+    tries += 1
     
+    
+    
+        
+   
 
-
+    //var  test = Int(readLine()!)
+    
+    //if guess == nil {
+       // print("yes")
+    //}
+    
+    
+    if tries >= 5 {
+        print("you ran out of tries the number was \(randemnumber) do you want to play again?")
+        print (" Do you play again" )
+        playAgain = readLine()!
+        }
+    if randemnumber != guess {
+        print("No")
+    }
+    if randemnumber == guess! {
+        print("yes")
+    }
+    
+    }
+    tries = 0
+}
 
 
